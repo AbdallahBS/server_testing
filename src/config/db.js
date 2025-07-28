cdcfc775f8f980f262f1b88b78d8704ede7e9f7c
Abdallah Ben Salem <abenssalem32@gmail.com>
2025-07-28 15:14:43 +0100

    second commit

--- src/config/db.js
+++ src/config/db.js
@@ -1,10 +1,10 @@
 const mongoose = require('mongoose');
 
-const dbURI =  'mongodb+srv://Abdallah:Azmidou142003@universe.1p0wuz9.mongodb.net/universe?retryWrites=true&w=majority';
+
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(dbURI);
+    await mongoose.connect(process.env.MONGODB_URI);
     console.log('Database connected successfully');
   } catch (error) {
     console.error('Database connection failed:', error);
